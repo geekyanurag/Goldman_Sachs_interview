1553216382757ef5f570c0372a7b288fa7a67de5
AnuragSaw <anurag.kumar@btech.christuniversity.in>
2019-08-11 00:06:29 +0530

    added insertion of node

--- delete_without_head_pointer.cpp
+++ delete_without_head_pointer.cpp
@@ -1,6 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+    }
+};
+
+Node* head;
+void insert(int data){
+    Node* temp = new Node(data);
+    temp->next = head;
+    head = temp;
+}
+
+void print(){
+    Node* temp = head;
+    while(temp != NULL){
+        cout<<temp->data;
+        temp = temp->next;
+    }
+}
 int main(){
-    
+    head = NULL;
+    for(int i =0; i<5; i++){
+        insert(i+1);
+    }
+
+    print();
 }
